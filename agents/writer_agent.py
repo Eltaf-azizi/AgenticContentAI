4e69311c61ea28555574d39a92515bf208d67cb0
Eltaf-azizi <iltfhuss@gmail.com>
2025-05-17 22:48:42 +0500

    working to write draft

--- agents/writer_agent.py
+++ agents/writer_agent.py
@@ -9,4 +9,28 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 
 def write_draft(topic: str, outline: str) -> str:
+    prompt = f"""
+    You are a professional content writer. Based on the topic and outline below,
+    write a full-length content draft make the tone engaging and informative.
+    Add transitions and make sure ot flows well.
+
+    Topic: {topic}
+
+    Outline
+    {outline}
+
+    Begin writting:
+    """
+    try:
+        response = client.chat.completions.create(
+            model = "gpt-4",
+            messages=[{"role": "user","content": prompt}],
+            temprature=0.7,
+            max_tokens=1200
+        )
+
+        return response.choices[0].message.content.strip()
     
+    except Exception as e:
+        return f"[Error] {str(e)}"
+
