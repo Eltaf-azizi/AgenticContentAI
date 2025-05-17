5d4cf05375ef1d9630f07e6b97066ed1e25636b6
Eltaf-azizi <iltfhuss@gmail.com>
2025-05-17 22:08:47 +0500

    working on the context and response

--- agents/outline_agent.py
+++ agents/outline_agent.py
@@ -7,10 +7,35 @@ load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 
-def generate_outline(topic: str, content: str) -> str:
+def generate_outline(topic: str, context: str) -> str:
     prompt = f"""
     You are a content strategist. Based on the following topic and context, generate a detailed outline for a blog or script.
 
 
     topic: {topic}
+
+    Context:
+    {context}
+
+    Your outline should have:
+    - Introduction
+    - 4 to 6 major sections with headings
+    - Bullet points under each section
+    - A conclusion
+
+    Make it clean and well-structured.
     """
+
+    try:
+        response = client.chat.completions.create(
+            model = "gpt-4",
+            messages=[{"role": "user","content": prompt}],
+            temprature=0.5,
+            max_tokens=800
+        )
+
+        return response.choices[0].message.content.strip()
+    
+    except Exception as e:
+        return f"[Error] {str(e)}"
+    
