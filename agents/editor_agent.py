1025ce52eecb203e4dc1d32c5412df9c99f50631
Eltaf-azizi <iltfhuss@gmail.com>
2025-05-18 16:08:13 +0500

    editing the content

--- agents/editor_agent.py
+++ agents/editor_agent.py
@@ -9,5 +9,29 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def edit_content(draftL str) -> str:
     prompt = f"""
-    
+    You are a professional content editor. Take the following draft and improve it by:
+    - Fixing grammar and puctuation
+    - Improving readability and tone
+    - Making it more engaging and conversational
+    - Enhancing SEO (without keyword stuffing)
+    - Keeping structure intact
+
+
+    Draft:
+    {draft}
+
+    return the polished version.
     """
+
+    try:
+        response = client.chat.completions.create(
+            model = "gpt-4",
+            messages=[{"role": "user","content": prompt}],
+            temprature=0.5,
+            max_tokens=1200
+        )
+
+        return response.choices[0].message.content.strip()
+    
+    except Exception as e:
+        return f"[Error] {str(e)}"
