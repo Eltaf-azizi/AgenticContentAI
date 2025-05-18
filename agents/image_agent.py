1c1770c8ba9c98fbf212a80549b1ceeade1b3ea2
Eltaf-azizi <iltfhuss@gmail.com>
2025-05-18 18:08:32 +0500

    working to generate image

--- agents/image_agent.py
+++ agents/image_agent.py
@@ -8,5 +8,19 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 
 def generate_image(topic: str) -> str:
+    prompt = f"A High Quality, Professional, AI-generated illustration for : {topic}"
 
+    try:
+        response = client.images.generate(
+            model = "dall-e-3",
+            prompt = prompt,
+            n = 1,
+            size = "1024x1024"
+        )
+
+        image_url = response.data[0].url
+        return image_url
+    
+    except Exception as e:
+        return f"[Error] {str(e)}"
     
