41b4d2619629489bd817b724e621a915adf750cf
Eltaf-azizi <iltfhuss@gmail.com>
2025-05-18 23:13:00 +0500

    worked on the markdown

--- utils/exporter.py
+++ utils/exporter.py
@@ -6,3 +6,19 @@ from fpdf import FPDF
 
 
 EXPORT_DIR = "static/exports"
+
+
+
+def save_as_markdown(content: str, topic: str) -> str:
+    os.makedirs(EXPORT_DIR, exit_ok=True)
+    filename = f"{topic.replace(' ', '_').lower()}.md"
+    filepath = os.path.join(EXPORT_DIR, filename)
+
+    with open(filepath, "w", encoding="utf-8") as f:
+        f.write(content)
+    return filename
+
+
+
+def save_as_pdf(content: str, topic: str) -> str:
+    os.makedirs(EXPORT_DIR, exit_ok=True)
