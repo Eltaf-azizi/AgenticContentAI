c2290db59409a4b92a97896623aa30bc004003a9
Eltaf-azizi <iltfhuss@gmail.com>
2025-05-20 19:18:49 +0500

    working on the langchain pipeline step by step

--- agents/langchain_pipeline.py
+++ agents/langchain_pipeline.py
@@ -13,3 +13,26 @@ from langchain_core.runnables import RunnableLambda
 
 
 # Step 1: Research
+research_chain = RunnableLambda(lambda x: {
+    "topic": x["topic"],
+    "context": research_tool.invoke(x["topic"])
+})
+
+
+
+# Step 2: Outline
+outline_chain = RunnableLambda(lambda x: {
+    "topic": x["topic"],
+    "outline": outline_tool.invoke({
+        "topic": x["topic"],
+        "context": x["context"]
+    })
+})
+
+
+
+
+# Step 3: Write Draft
+write_chain = RunnableLambda(lambda x: {
+    
+})
