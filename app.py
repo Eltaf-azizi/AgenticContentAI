f884df6c0c56e46f031305341fe36b6b838d3a76
Eltaf-azizi <iltfhuss@gmail.com>
2025-05-21 20:22:28 +0500

    working on the index

--- app.py
+++ app.py
@@ -11,4 +11,25 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if 
+    if request.method == "POST":
+        topic = request.form.get("topic")
+        content_type = request.form.get("content_type")
+        tone = request.form.get("tone")
+        pipeline = request.form.get("pipeline") # pipeline toggle
+
+
+        combined_topic = f"{topic} ({content_type})"
+
+
+
+        # Toggle between LangGraph and Classic Chain
+        if pipeline == "chain":
+            result = chain.invoke({
+                "topic": combined_topic,
+                "tone": tone
+            })
+        else:
+            result = graph.invoke({
+                "topic": combined_topic,
+                "tone": tone
+            })
